9599fdbf949a7b577d84ac2660654a413a2e9e35
AFROZ <2006004@kiit.ac.in>
2026-01-22 15:12:01 +0530

    Add card

---
+++ FoodieDash/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using FoodieDash.Data;
+using FoodieDash.Models;
+using FoodieDash.Utility; // Import the helper we just made
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodieDash.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // 1. Get the current list of items from the Session
+            List<MenuItem> cart = HttpContext.Session.GetObject<List<MenuItem>>("MyCart") ?? new List<MenuItem>();
+
+            // 2. Pass it to the view
+            return View(cart);
+        }
+
+        public IActionResult Add(int id)
+        {
+            // 1. Find the product in the database
+            MenuItem? itemToAdd = _context.MenuItems.Find(id);
+
+            if (itemToAdd != null)
+            {
+                // 2. Get the existing cart (or create a new one if empty)
+                List<MenuItem> cart = HttpContext.Session.GetObject<List<MenuItem>>("MyCart") ?? new List<MenuItem>();
+
+                // 3. Add the new item
+                cart.Add(itemToAdd);
+
+                // 4. Save the updated list back to Session
+                HttpContext.Session.SetObject("MyCart", cart);
+            }
+
+            // 5. Go back to Home Page
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ FoodieDash/Utility/SessionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace FoodieDash.Utility
+{
+    public static class SessionExtensions
+    {
+        // Save an object (like a Cart) into Session
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        // Retrieve an object from Session
+        public static T? GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
